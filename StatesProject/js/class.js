b84beb963da214c0983282de5e1316b04343e03a
ItayMizrachi <125411823+ItayMizrachi@users.noreply.github.com>
2023-02-18 17:52:10 +0200

    Add files via upload

--- StatesProject/js/class.js
+++ StatesProject/js/class.js
@@ -1,4 +1,4 @@
-export default class Class {
+export default class Country {
     constructor(_parent, _item, stateBorders, doApi) {
         this.parent = _parent;
         this.name = _item.name.common;
@@ -16,43 +16,47 @@ export default class Class {
         this.stateBorders = stateBorders;
     }
 
-    render() {
-        let div = document.createElement("div");
+    async render() {
+        const div = document.createElement("div");
         div.className = "col-md-8 mx-auto p-4 conic text-white";
         document.querySelector(this.parent).append(div);
 
         div.innerHTML = `
-        <img  src="${this.flag}"  alt="${this.name}" class="ms-4 w-50 float-end  ">
+        <img src="${this.flag}" alt="${this.name}" class="ms-4 w-50 float-end">
         <h2 style="color: #5bc0de;">${this.name}</h2>
         <div><strong>POP</strong>: ${this.pop} </div>
         <div><strong>Region</strong>: ${this.region}</div>
         <div><strong>Languages</strong>: ${this.language}</div>
-        <div><strong>Coin:</strong> ${this.coin}, ${this.coinDescription}  ${this.coinSymbol}</div>
+        <div><strong>Coin:</strong> ${this.coin}, ${this.coinDescription} ${this.coinSymbol}</div>
         <div><strong>Capital:</strong> ${this.capital}</div>
-        <div class="mt-3 "><strong>States with borders:</strong><br>
-        <div class="borders_div"></div>
+        <div class="mt-3"><strong>States with borders:</strong><br>
+          <div class="bordersDiv"></div>
         </div>
-        
-        <iframe class="mt-4 col-12" height="600" src="https://maps.google.com/maps?q=${this.map[0]},${this.map[1]}&z=6&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ></iframe>
-       `
+        <iframe class="mt-4 col-12" height="600" src="https://maps.google.com/maps?q=${this.map[0]},${this.map[1]}&z=6&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      `;
 
-        let borders_div = div.querySelector(".borders_div");
-        this.borders.forEach(async (item, index) => {
-            let a = document.createElement("a");
-            a.innerHTML = await this.stateBorders(item) + " ";
-            a.style = "font-weight: bold; font-size: 1.1em; color: #5bc0de; cursor: pointer; padding: 2px; "
-            borders_div.append(a);
+        const bordersDiv = div.querySelector(".bordersDiv");
+
+        for (const [index, item] of this.borders.entries()) {
+            const text = await this.stateBorders(item);
+            const a = document.createElement("a");
+            a.textContent = text;
+            a.style.fontWeight = "bold";
+            a.style.fontSize = "1.1em";
+            a.style.color = "#5bc0de";
+            a.style.cursor = "pointer";
+            a.style.padding = "2px";
+            bordersDiv.append(a);
 
             if (index !== this.borders.length - 1) {
-                let comma = document.createElement("span");
-                comma.innerHTML = ", ";
-                borders_div.append(comma);
+                const comma = document.createElement("span");
+                comma.textContent = ", ";
+                bordersDiv.append(comma);
             }
 
             a.addEventListener("click", () => {
-                this.doApi(a.innerHTML);
-            })
-        })
-
+                this.doApi(text);
+            });
+        }
     }
 }
